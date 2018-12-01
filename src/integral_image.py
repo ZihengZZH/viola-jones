cc92201c9895187c117f0ac4be031e82a6c88726
ZihengZZH <zihengzhang1025@gmail.com>
2018-12-01 13:46:00 +0000

    haar features & test

--- src/integral_image.py
+++ src/integral_image.py
@@ -4,9 +4,13 @@ import numpy as np
 # It is produced by cumulative addition of intensities on subsequent pixels in horizontal and vertical axis
 # It also greatly reduce the computation of features
 
+
 class IntegralImage(object):
+    # Class for getting integral image
+    # attribute int_img is the calculated integral image of a bigger size than original
+
     def __init__(self, img):
-        self.shape = img.shape
+        self.shape = (img.shape[0]+1, img.shape[1]+1)
         self.img = img
         # integral image to be calculated
         self.int_img = np.ones(self.shape)
@@ -14,20 +18,48 @@ class IntegralImage(object):
         self.memo = np.zeros(self.shape)
         self.get()
 
-    # Calculate value of each pixel
     def calc(self, x, y):
-        if x < 0 or y < 0:
+        # Calculate value of each pixel
+        if x == 0 or y == 0:
             return 0
         # if already calc, return value
         if self.memo[x][y] == 1:
             return self.int_img[x][y]
         else:
             # principal equation
-            cummulative = self.calc(x-1, y) + self.calc(x, y-1) - self.calc(x-1, y-1) + self.img[x][y]
+            cummulative = self.calc(
+                x-1, y) + self.calc(x, y-1) - self.calc(x-1, y-1) + self.img[x-1][y-1]
             self.memo[x][y] = 1
             return cummulative
 
     def get(self):
+        # Get the integral image with additional rows/cols of 0
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
                 self.int_img[i][j] = self.calc(i, j)
+
+
+def get_sum(int_img, top_left, bottom_right):
+    # get summed value over a rectangle (attention to the tuple)
+
+    top_left = (top_left[1], top_left[0])
+    bottom_right = (bottom_right[1], bottom_right[0])
+    # must swap the tuples since the orientation of the coordinate system
+    if top_left == bottom_right:
+        return int_img[top_left]
+    top_right = (bottom_right[0], top_left[1])
+    bottom_left = (top_left[0], bottom_right[1])
+    
+    return int_img[bottom_right] + int_img[top_left] - int_img[bottom_left] - int_img[top_right]
+
+
+'''
+Note that the orientations of the coordiante system are opposite
+-------------------> x
+|   x1,y1   x2,y1
+|   x1,y2   ...
+|   ...
+|
+y
+
+'''
--- test/integral_image_test.py
+++ test/integral_image_test.py
@@ -1,36 +1,44 @@
 import unittest
 from src.integral_image import IntegralImage as integral
+from src.integral_image import get_sum
 import numpy as np
 from PIL import Image
-import cv2
 
 '''
 unittest: The Python unit testing framework
 unittest supports test automation, sharing of setup and shutdown code for tests, aggregation of tests into collections, and independence of the tests from the reporting framework.
-
-note that opencv library is only used to test integral image calculation
+to run the unittest, python -m unittest test.integral_image_test
 '''
 
+
 class IntegralImageTest(unittest.TestCase):
-    # method called to prepare the test fixture
+
     def setUp(self):
-        self.ori_img = np.array(Image.open('./train_images/FACES/face00001.bmp'), dtype=np.float64)
+        # method called to prepare the test fixture
+        self.ori_img = np.array(Image.open(
+            './train_images/FACES/face00001.bmp'), dtype=np.float64)
         self.int_img = integral(self.ori_img).int_img
 
-    # method called immediately after the test method has been called and the result recorded
     def tearDown(self):
+        # method called immediately after the test method has been called and the result recorded
         pass
 
     def test_integral_calculation(self):
-        assert self.int_img[0,0] == self.ori_img[0,0] # top-left corner
-        assert self.int_img[-1,0] == np.sum(self.ori_img[:,0]) # bottom-left corner
-        assert self.int_img[0,-1] == np.sum(self.ori_img[0,:]) # top-right corner
-        assert self.int_img[-1,-1] == np.sum(self.ori_img) # bottom-right corner
+        # top-left corner
+        assert self.int_img[1, 1] == self.ori_img[0, 0] 
+        # bottom-left corner
+        assert self.int_img[-1, 1] == np.sum(self.ori_img[:, 0])
+        # top-right corner
+        assert self.int_img[1, -1] == np.sum(self.ori_img[0, :])
+        # bottom-right corner
+        assert self.int_img[-1, -1] == np.sum(self.ori_img)
 
-    def test_with_opencv(self):
-        int_img_cv2 = cv2.integral(self.ori_img) # opencv built-in function
-        assert self.int_img.any() == int_img_cv2[1:,1:].any()
+    def test_get_sum(self):
+        # pay attention that integral image has additional rows or columns of 0
+        assert get_sum(self.int_img, (0, 0), (1, 1)) == self.ori_img[0, 0]
+        assert get_sum(self.int_img, (0, 0), (-1, -1)) == np.sum(self.ori_img)
 
-# a command-line program that loads a set of tests from integral_image and runs them
+
 if __name__ == "__main__":
+    # a command-line program that loads a set of tests from integral_image and runs them
     unittest.main()
